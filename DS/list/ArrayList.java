a44ce51efdf0dc583c779db873883e1c560b4aca
Minseok <minseok05423@gmail.com>
2025-09-13 19:02:41 +0900

    list1

--- DS/list/ArrayList.java
+++ DS/list/ArrayList.java
@@ -1,11 +1,23 @@
 package DS.list;
 
 public class ArrayList<E> implements ListInterface<E> {
-    private E items[];
+    private E item[];
     private int numItems;
     private static final int DEFAULT_CAPACITY = 64;
 
     public ArrayList() {
-        item = (E[]) new Object
+        item = (E[]) new Object[DEFAULT_CAPACITY];
+        numItems = 0;
+    }
+
+    public ArrayList(int n) {
+        item = (E[]) new Object[n];
+        numItems = 0;
+    }
+
+    public void add(int index, E x) {
+        if (numItems >= item || index < 0 || index > numItems) {
+            
+        }
     }
 }
