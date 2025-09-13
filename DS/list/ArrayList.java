e0108122e849d5209e3b7ed5245e2ad5c661d14d
Minseok <minseok05423@gmail.com>
2025-09-13 18:55:02 +0900

    list0

---
+++ DS/list/ArrayList.java
@@ -0,0 +1,11 @@
+package DS.list;
+
+public class ArrayList<E> implements ListInterface<E> {
+    private E items[];
+    private int numItems;
+    private static final int DEFAULT_CAPACITY = 64;
+
+    public ArrayList() {
+        item = (E[]) new Object
+    }
+}
