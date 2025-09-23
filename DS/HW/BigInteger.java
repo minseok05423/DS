d67e628cecb81d7f1b72a9142fd781afef2b3610
Minseok <minseok05423@gmail.com>
2025-09-23 18:03:17 +0900

    fixed subtraction logic

--- DS/HW/BigInteger.java
+++ DS/HW/BigInteger.java
@@ -13,10 +13,35 @@ public class BigInteger
     public static final Pattern EXPRESSION_PATTERN = Pattern.compile("(\\d+)\\s*([\\+\\-\\*])\\s*(\\d+)");
     
     private int[] digits;
-    private boolean isNegative;
+    private boolean isNegative = false;
   
-    public BigInteger(int i)
+    public BigInteger(int i) 
     {
+        if (i < 0) {
+            isNegative = true;
+            i = -i;
+        } else {
+            isNegative = false;
+        }
+
+        if (i == 0) {
+            digits = new int[1];
+            digits[0] = 0;
+            return;
+        }
+
+        int temp = i;
+        int count = 0;
+        while (temp > 0) {
+            temp /= 10;
+            count++;
+        }
+
+        digits = new int[count];
+        for (int idx = count-1; idx >= 0; idx--) {
+            digits[idx] = i % 10;
+            i /= 10;
+        }
     }
   
     public BigInteger(int[] num1)
@@ -61,25 +86,38 @@ public class BigInteger
             result[resultLength] = carry;
         }
 
-        return new BigInteger(result);
+        int[] refinedResult = refine(result);
+        BigInteger finalResult = new BigInteger(refinedResult);
+        finalResult.isNegative = (this.isNegative == true);
+
+        return finalResult;
     }
     
     public BigInteger subtract(BigInteger big) {
+        int[] num1;
+        int[] num2;
+
+        int[] bigger = absCompare(this, big);
+
+        if ((this.isNegative == true) == (bigger == this.digits)) {
+            num1 = this.digits;
+            num2 = big.digits;
+        }
+        else {
+            num1 = big.digits;
+            num2 = this.digits;
+        }    
+
     // assume this >= big and both are positive
-        int[] result = new int[this.digits.length];
+        int maxLength = Math.max(num1.length, num2.length);
+        int[] result = new int[maxLength];
         int borrow = 0;
 
-        for (int i=0; i<this.digits.length; i++) {
+        for (int i=0; i<maxLength; i++) {
             int thisDigit = (i < this.digits.length) ? this.digits[i] : 0;
             int bigDigit = (i < big.digits.length) ? big.digits[i] : 0;
 
             int calculation = thisDigit - bigDigit - borrow;
-            System.out.println(this);
-            System.out.println(big);
-            System.out.println(thisDigit);
-            System.out.println(bigDigit);
-            System.out.println(borrow);
-            System.out.println(calculation);
 
             if (calculation >= 0) {
                 result[i] = calculation;
@@ -89,51 +127,23 @@ public class BigInteger
                 borrow = 1;
             }
         }
-        return new BigInteger(result);   
+        int[] refinedResult = refine(result);
+        BigInteger finalResult = new BigInteger(refinedResult);
+
+        if (bigger != num1) {
+            finalResult.isNegative = true;
+        }
+
+        return finalResult;   
     }
-  public static void main(String[] args) {
-      // Test basic construction and toString
-      System.out.println("=== Testing Construction and toString ===");
-      BigInteger num1 = new BigInteger("429");
-      BigInteger num2 = new BigInteger("123");
-      System.out.println("num1: " + num1.toString());
-      System.out.println("num2: " + num2.toString());
 
-      // Test addition
-      System.out.println("\n=== Testing Addition ===");
-      BigInteger sum = num1.add(num2);
-      System.out.println("123 + 456 = " + sum.toString());
-
-      // Test subtraction (assuming first >= second)
-      System.out.println("\n=== Testing Subtraction ===");
-      BigInteger diff = num2.subtract(num1);
-      System.out.println("123 - 429 = " + diff.toString());
-
-      // Test multiplication
-      System.out.println("\n=== Testing Multiplication ===");
-      BigInteger prod = num1.multiply(num2);
-      System.out.println("123 * 456 = " + prod.toString());
-
-      // Test negative numbers
-      System.out.println("\n=== Testing Negative Numbers ===");
-      BigInteger neg = new BigInteger("-123");
-      System.out.println("Negative: " + neg.toString());
-
-      // Test small numbers
-      System.out.println("\n=== Testing Small Numbers ===");
-      BigInteger small1 = new BigInteger("5");
-      BigInteger small2 = new BigInteger("3");
-      System.out.println("5 + 3 = " + small1.add(small2).toString());
-      System.out.println("5 - 3 = " + small1.subtract(small2).toString());
-      System.out.println("5 * 3 = " + small1.multiply(small2).toString());
-  }
-  
     public BigInteger multiply(BigInteger big) {
+        int maxLength = Math.max(this.digits.length, big.digits.length);
         // assume this >= big
         int[] result = new int[this.digits.length + big.digits.length];
         int carry = 0;
 
-        for (int i=0; i<big.digits.length; i++) {
+        for (int i=0; i<maxLength; i++) {
             for (int k=0; k<this.digits.length; k++) {
                 int product = big.digits[i] * this.digits[k] + carry;
                 result[i + k] = product % 10;
@@ -145,10 +155,44 @@ public class BigInteger
             result[this.digits.length + big.digits.length - 1] = carry;
         }
 
-        BigInteger finalResult = new BigInteger(result);
+        int[] refinedResult = refine(result);
+        BigInteger finalResult = new BigInteger(refinedResult);
         finalResult.isNegative = (this.isNegative != big.isNegative);
+
         return finalResult;
     }
+
+    public int[] absCompare(BigInteger num1, BigInteger num2) {
+        int[] digit1 = num1.digits;
+        int[] digit2 = num2.digits;
+        if (digit1.length > digit2.length) {
+            return digit1;
+        }
+        if (digit1.length < digit2.length) {
+            return digit2;
+        }
+        else {
+            for (int i = 0; i < digit1.length; i++) {
+                if (digit1[i] > digit2[i]) {
+                    return digit1;
+                }
+                if (digit2[i] > digit1[i]) {
+                    return digit2;
+                }
+            }
+            return digit1;
+        }
+    }
+
+    public int[] refine(int[] array) {
+        int[] result = new int[array.length];
+        for (int i=0; i<array.length; i++) {
+            if (array[i] > 0) {
+                result[i] = array[i];
+            }
+        }
+        return result;
+    } 
   
     @Override
     public String toString()
@@ -177,30 +221,22 @@ public class BigInteger
             switch (operator) {
                 case "+":
                     if (num1.isNegative == num2.isNegative) {
-                        BigInteger result = num1.add(num2);
-                        result.isNegative = num1.isNegative;
-                        return result;
+                        return num1.add(num2);
                     } else {
-                        if (num1.isNegative) {
-
-                            BigInteger result = num2.add(num1);
-                            result.isNegative = num1.isNegative;
-                            return result;
-                        } else {
-
-                        }
+                        return num1.subtract(num2);
                     }
-                    break;
                 case "-":
-                    
-                    break;
+                    if (num1.isNegative == num2.isNegative) {
+                        return num1.subtract(num2);
+                    } else {
+                        return num1.add(num2);
+                    }
                 case "*":
-                    
-                    break;
+                    return num1.multiply(num2);
                 default:
                     throw new AssertionError();
+                }
             }
-
 
         }
   
